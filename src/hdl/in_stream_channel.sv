f4410a28157d22895eb8ff91c3504a8da0f22b72
Tom Hagel <hageltom@azula.cs.uni-tuebingen.de>
2026-03-02 23:17:29 +0100

    enhance to skid buffer

--- src/hdl/in_stream_channel.sv
+++ src/hdl/in_stream_channel.sv
@@ -14,19 +14,44 @@ module in_stream_channel #(
   output logic valid_o,
   input logic ready_i
 );
+  logic [DATA_WIDTH-1:0] data_reg;
+  logic reg_valid;
 
-  assign ready_o = ready_i || !valid_o;
+  logic [DATA_WIDTH-1:0] skid_reg;
+  logic skid_valid;
+
+  assign ready_o = ~skid_valid;
+
+  assign data_o = data_reg;
+  assign valid_o = reg_valid;
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      data_o <= 'b0;
-      valid_o <= 1'b0;
+      data_reg <= 'b0;
+      reg_valid <= 1'b0;
+      skid_reg <= 'b0;
+      skid_valid <= 1'b0;
     end else begin
-      if (ready_o) begin
-        valid_o <= valid_i;
-
-        if (valid_i) begin
-          data_o <= data_i;
+      if (ready_i) begin
+        if (skid_valid) begin
+          data_reg <= skid_reg;
+          reg_valid <= 1'b1;
+          skid_valid <= 1'b0;
+        end else if (valid_i && ready_o) begin
+          data_reg <= data_i;
+          reg_valid <= 1'b1;
+        end else begin
+          reg_valid <= 1'b0;
+        end
+      end else begin
+        if (valid_i && ready_o) begin
+          if (!reg_valid) begin
+            data_reg <= data_i;
+            reg_valid <= 1'b1;
+          end else begin
+            skid_reg <= data_i;
+            skid_valid <= 1'b1;
+          end
         end
       end
     end
